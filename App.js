6c94d0daf9c168e63c7f7c8ef7d51d6872b5097b
Alexander Hogle <alexander.hogle@gmail.com>
2021-09-05 14:03:55 -0500

    adds viewimagescreen

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { StyleSheet, Dimensions, Text, TouchableHighlight, View, Image, Platform, SafeAreaView, Alert, Button, StatusBar } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   const {landscape} = useDeviceOrientation();
 
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
