e21d862442cc332d7ac10690ff65d9258b2bb531
Alexander Hogle <alexander.hogle@gmail.com>
2021-09-05 11:45:01 -0500

    adds images

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import { StyleSheet, Dimensions, Text, TouchableHighlight, View, Image, Platform, SafeAreaView, Alert, Button, StatusBar } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   const {landscape} = useDeviceOrientation();
 
   return (
-    <View  style={{ backgroundColor: '#fff',
-     flex: 1, 
-     flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    alignContent: "center",
-    flexWrap: "wrap" }}>
-      <View style={{ backgroundColor: 'dodgerblue',
-      flexBasis: 100,
-      // width: 100,
-      height: 100, }}/>
-      <View style={{ backgroundColor: 'gold', width: 100, height: 100, }}/>
-      <View style={{ backgroundColor: 'tomato', width: 100, height: 100 }}/> 
-    </View>
+    <View></View>
   );
 }
 
