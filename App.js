7dc4b0cda8e9567f3b46134f0dcd610d46c4e226
Alexander Hogle <alexander.hogle@gmail.com>
2021-09-07 12:52:07 -0500

    adds Messages Screen

--- App.js
+++ App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import { StyleSheet, Dimensions, Text, TouchableHighlight, View, Image, Platform, SafeAreaView, Alert, Button, StatusBar } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
+import React from "react";
+import {
+  StyleSheet,
+  Dimensions,
+  Text,
+  TouchableHighlight,
+  View,
+  Image,
+  Platform,
+  SafeAreaView,
+  Alert,
+  Button,
+  StatusBar,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
-import AppText from './app/components/AppText/AppText';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import AppText from "./app/components/AppText/AppText";
+import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return (
-    <ViewImageScreen />
-  );
+  return <MessagesScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
