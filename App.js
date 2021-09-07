a21c0e8685634a6905e93fd92a52d73e24d7b33a
Alexander Hogle <alexander.hogle@gmail.com>
2021-09-06 19:58:01 -0500

    adds card component

--- App.js
+++ App.js
@@ -7,10 +7,20 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 
 import AppText from './app/components/AppText/AppText';
 import AppButton from './app/components/AppButton';
+import Card from './app/components/Card';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <View style={{
+      backgroundColor: '#f8f4f4',
+      padding: 20,
+      paddingTop: 100
+    }}>
+      <Card
+        title="Red jacket for sale"
+        subTitle="$100"
+        image={require('./app/assets/jacket.jpg')} />
+    </View>
   );
 }
 
