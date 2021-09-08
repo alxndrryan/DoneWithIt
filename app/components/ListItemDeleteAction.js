13073eb1504b63b94c51a22505d586fe3fcf6025
Alexander Hogle <alexander.hogle@gmail.com>
2021-09-08 13:26:54 -0500

    deletes list items

--- app/components/ListItemDeleteAction.js
+++ app/components/ListItemDeleteAction.js
@@ -3,11 +3,18 @@ import { View, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import colors from "../config/colors";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
-function ListItemDeleteAction(props) {
+function ListItemDeleteAction({ onPress }) {
   return (
     <View style={styles.container}>
-      <MaterialCommunityIcons name="trash-can" size={35} color={colors.white} />
+      <TouchableWithoutFeedback onPress={onPress}>
+        <MaterialCommunityIcons
+          name="trash-can"
+          size={35}
+          color={colors.white}
+        />
+      </TouchableWithoutFeedback>
     </View>
   );
 }
