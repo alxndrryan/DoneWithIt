d74bcb56ed106528c91e9f2d628bc0b120f18ed2
Alexander Hogle <alexanderhogle@gmail.com>
2021-09-09 10:20:03 -0500

    styling

--- app/components/AppText/styles.js
+++ app/components/AppText/styles.js
@@ -1,11 +1,10 @@
 const { StyleSheet, Platform } = require("react-native");
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 18,
-        fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir'
-
-    }
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
 });
 
 export default styles;
