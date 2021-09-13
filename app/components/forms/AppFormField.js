de34572290f29a9fc7803c4d1588b6bace7cc750
Alexander Hogle <alexanderhogle@gmail.com>
2021-09-13 13:05:55 -0500

    fixed icons on login screen

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -4,7 +4,7 @@ import { useFormikContext } from "formik";
 import AppTextInput from "../AppTextInput";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormField({ name, otherProps }) {
+function AppFormField({ name, ...otherProps }) {
   const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
 
   return (
