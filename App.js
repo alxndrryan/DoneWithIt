66ed8ecd57a81b49516884b4f374140a8e49fffa
Alexander Hogle <alexanderhogle@gmail.com>
2021-09-14 12:26:12 -0500

    handles long text in cards and apptext components

--- App.js
+++ App.js
@@ -38,5 +38,5 @@ import RegisterScreen from "./app/screens/RegisterScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingsScreen />;
 }
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -19,6 +19,14 @@ const initialMessages = [
     description: "D2",
     image: require("../assets/alex.png"),
   },
+  {
+    id: 3,
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    image: require("../assets/alex.png"),
+  },
 ];
 
 function MessagesScreen(props) {
