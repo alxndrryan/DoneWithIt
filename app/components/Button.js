2af398d7e7f552dd55d6e34b011b28698192cf3e
Alexander Hogle <alexanderhogle@gmail.com>
2021-10-01 12:16:36 -0500

    working with react navigation

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,33 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
-import colors from '../config/colors';
+import colors from "../config/colors";
 
-function AppButton({title, onPress, color = 'primary' }) {
-    return (        
-        <TouchableOpacity style={[styles.button, { backgroundColor: colors[color]}]} onPress={onPress}>
-                <Text style={styles.text}>{title}</Text>
-        </TouchableOpacity>
-       
-    );
+function AppButton({ title, onPress, color = "primary" }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: colors[color] }]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        alignItems: 'center',
-        backgroundColor: colors.primary,
-        borderRadius: 25,
-        justifyContent: 'center',
-        marginVertical: 10,
-        padding: 15,
-        width: '100%',
-    },
-    text: {
-        color: colors.white,
-        fontSize: 18,
-        fontWeight: 'bold',
-        textTransform: 'uppercase'
-    }
-})
+  button: {
+    alignItems: "center",
+    backgroundColor: colors.primary,
+    borderRadius: 25,
+    justifyContent: "center",
+    marginVertical: 10,
+    padding: 15,
+    width: "100%",
+  },
+  text: {
+    color: colors.white,
+    fontSize: 18,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+});
 
 export default AppButton;
